e274607adbbfff4fddcba2374cd1f6f0f61866cc
Ngom <130662339+Cngom99@users.noreply.github.com>
2024-12-27 04:41:49 +0000

    premier commit

---
+++ Projet-gestion-universitaire/app/database.php
@@ -0,0 +1,12 @@
+<?php
+    $host = 'localhost';
+    $db = 'gestion_universitaire';
+    $user = 'postgres';
+    $password = 'root';
+
+    $conn = pg_connect("host=$host dbname=$db user=$user password=$password");
+
+    if (!$conn) {
+        die("Connection failed: " . pg_last_error());
+    }
+?>
